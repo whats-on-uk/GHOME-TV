d2e3403ac1598506766b45fd6f73fa03e2c6b05b
ksouthern <karl.southern@durham.ac.uk>
2019-02-04 20:39:33 +0000

    Karl playing around with flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/test/')
+def hello_world_test():
+    return 'Hello World! - test'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    print(request)
+    request_text = ""
+    for key in request.args.keys():
+        request_text += str(key)+":"+request.args[key]+"."
+    return request_text
 
 
 if __name__ == '__main__':
